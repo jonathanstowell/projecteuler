5963fc2e6632d191f9c6319b3ae959603f2bd205
Jon Stowell <jons@illycorp.com>
2012-02-21 13:19:49 +0000

    Natural Numbers.

---
+++ JS.ProjectEuler.NaturalNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JS.ProjectEuler.NaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.Out.WriteLine(string.Format("Sum of natural numbers 3 and 5 is: {0}", sum));
+            Console.Read();
+        }
+    }
+}
